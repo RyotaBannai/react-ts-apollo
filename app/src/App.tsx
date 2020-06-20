6da236eb6c2fcb8284194f3f46b8069e0b6df4ea
RyotaBannai <ryotala0528@gmail.com>
2020-06-20 20:28:09 +0900

    apollo fetch exhanges rates from usd

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+import './App.css';
 import { Template } from './templates/Template'
 import { Main } from "./pages/Main";
 
-function App() {
-  return (
-    <div className="App">
-        <Router>
-            <Template>
-                <Route exact path="/" component={ Main } />
-            </Template>
-        </Router>
+const client = new ApolloClient({
+    uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
 
-    </div>
+export default function App() {
+  return (
+      <ApolloProvider client={client}>
+        <div className="App">
+            <Router>
+                <Template>
+                    <Route exact path="/" component={ Main } />
+                </Template>
+            </Router>
+
+        </div>
+      </ApolloProvider>
   );
 }
-
-export default App;
--- app/src/organisms/Apollo.tsx
+++ app/src/organisms/Apollo.tsx
@@ -1,15 +1,31 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from '@apollo/react-hooks';
 
 interface Props {
     title: string
 }
 
+const EXCHANGE_RATES = gql`
+  {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
 
 export const Apollo: React.FC<Props> = ({title}) => {
-    useEffect(()=>{ }, []);
+    const { loading, error, data } = useQuery(EXCHANGE_RATES);
+    useEffect(()=>{
+
+    }, []);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
     return (
         <div>
-            <h2>{title}</h2>
+            { data.rates.map((rate: any) => <p> {rate.currency} </p>)}
         </div>
     );
 };
